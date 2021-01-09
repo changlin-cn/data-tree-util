ac84134999a1be558bb6dcfe2c3efb13e6c1cf94
Hechanglin <542177932@qq.com>
2021-01-09 20:42:37 +0800

    getPathFromTree 添加类型

--- src/get-path-from-tree.ts
+++ src/get-path-from-tree.ts
@@ -1,18 +1,16 @@
-const defaultGetPathFromTreeOpt = {
-  childrenKey: 'children',
-  isIt: (item, id) => item.id === id,
-};
+import { treeNode } from './tree-to-array';
 
-export function getPathFromTree(arr = [], id, option = defaultGetPathFromTreeOpt) {
-  const { childrenKey, isIt } = option;
-  const queue = [{ item: { [childrenKey]: arr }, path: '' }];
+export function getPathFromTree<T>(arr: treeNode<T>[], id: string | number): number[] {
+  const queue: { item: treeNode<T>; path: string }[] = arr.map((n, i) => ({ item: n, path: `${i}` }));
+
   while (queue.length) {
-    const current = queue.shift();
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const current = queue.shift()!;
     const {
-      item: { [childrenKey]: children },
+      item: { children },
       path,
     } = current;
-    if (isIt(current.item, id) && path) {
+    if (current.item.id === id && path) {
       return path.split('-').map((v) => Number(v));
     }
     if (Array.isArray(children) && children.length) {
@@ -27,4 +25,4 @@ export function getPathFromTree(arr = [], id, option = defaultGetPathFromTreeOpt
   throw new Error(`Can not get path of ${id}`);
 }
 
-export { getPathFromTree };
+console.log(getPathFromTree([{ id: 3, parentId: undefined, children: [{ id: 31, parentId: 3 }] }], 31));
